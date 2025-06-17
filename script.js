89b8626d738610531df211d6d349854f0045d28e
Buchannagari Lokesh <blokesh0205@gmail.com>
2025-06-17 13:45:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Alert interaction
+document.getElementById("alertBtn").addEventListener("click", () => {
+    alert("👋 Hello Lokesh! Keep pushing your web dev skills forward!");
+  });
+  
+  // Dark mode toggle
+  document.getElementById("themeToggle").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
+  
